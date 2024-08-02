34eec99d648a0eb2f8a80d066d08ffb886bc2e7c
a-levitt <artemis.levitt@gmail.com>
2024-08-02 15:31:08 +0200

    Add data provider for providing several books data

--- src/test/java/alevitt/DynamicJson.java
+++ src/test/java/alevitt/DynamicJson.java
@@ -5,22 +5,23 @@ import files.Payload;
 import files.ReusableMethods;
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
+//import static org.hamcrest.Matchers.equalTo;
 
 public class DynamicJson {
 
-    @Test
-    public void addBook() {
+    @Test(dataProvider = "BooksData")
+    public void addBook(String isbn, String aisle) {
         RestAssured.baseURI = "http://216.10.245.166";
 
         String response =
         given()
                 //.log().all()
                 .header("Content-Type", "application/json")
-                .body(Payload.addBookBody("bcd", "667o510"))
+                .body(Payload.addBookBody(isbn, aisle))
         .when()
                 .post("Library/Addbook.php")
         .then()
@@ -34,6 +35,11 @@ public class DynamicJson {
         JsonPath js = ReusableMethods.rawToJson(response);
 
         String newBookID = js.getString("ID");
-        System.out.println(newBookID);
+        System.out.println("New book created with ID: " + newBookID);
+    }
+
+    @DataProvider(name="BooksData")
+    public Object[][] getData() {
+        return new Object[][] {{"bdd", "666o510"}, {"tdd", "667o510"}, {"grpc", "668o51o"}};
     }
 }
